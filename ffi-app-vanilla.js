19c39fd9240e553fb4f692102d7816ae0ddda8d8
tnelligan <trevornelligan@yahoo.com>
2015-08-14 11:54:17 -0500

    added json examples and fixed up the ffi-lib
    added examples of json strings to send as messages from the client, and
    make the ffi-lib more streamlined

--- ffi-app-vanilla.js
+++ ffi-app-vanilla.js
@@ -16,36 +16,31 @@ var wss = new WebSocketServer({
 });
 
 // define C types to that will be used as arguments for FFI
-//TODO: tnelligan either get rid of these and replace with strings with asterisk, or explain whey these are here
-var NIint32 = ref.refType('int');
-var ptrChar = ref.refType(ref.types.CString);
-var TaskHandle = ref.refType('void');
-var ReadData = ref.refType('int32');
-
+// These are neccesary because there is no string representation of these by default
 var Double = ref.types.double;
 var DoubleArray = ArrayType(Double);
 
-var EveryNCallback = ffi.Function('int32', [TaskHandle, 'int32', 'uint32', TaskHandle]);
+var EveryNCallback = ffi.Function('int32', ['void*', 'int32', 'uint32', 'void*']);
 
 
 // Foreign Function Library
 
 //TODO: add checking to see if they have the driver
 var niDAQmx = ffi.Library('nicaiu', {
-  'DAQmxGetDevAIPhysicalChans': ['int32', ['string', ptrChar, 'ulong']],
-  'DAQmxGetSysDevNames': ['int32', [ptrChar, 'int32']],
-  'DAQmxGetSysNIDAQMajorVersion': ['int32', [NIint32]],
-  'DAQmxGetSysNIDAQMinorVersion': ['int32', [NIint32]],
-  'DAQmxCreateTask': ['int32', ['string', TaskHandle]],
-  'DAQmxCreateAIVoltageChan': ['int32', [TaskHandle, 'string', 'string', 'int32', 'double', 'double', 'int32', 'string']],
-  'DAQmxCreateDOChan': ['int32', [TaskHandle, 'string', 'string', 'int32']],
-  'DAQmxCfgSampClkTiming': ['int32', [TaskHandle, 'string', 'double', 'int32', 'int32', 'uint64']],
-  'DAQmxStartTask': ['int32', [TaskHandle]],
-  'DAQmxReadAnalogF64': ['int32', [TaskHandle, 'int32', 'double', 'int32', DoubleArray, 'uint32', ReadData, 'uint32']],
-  'DAQmxWriteDigitalScalarU32': ['int32', [TaskHandle, 'bool', 'double', 'int32', 'bool*']],
-  'DAQmxReadDigitalScalarU32': ['int32', [TaskHandle, 'double', 'int32*', 'bool*']],
-  'DAQmxRegisterEveryNSamplesEvent': ['int32', [TaskHandle, 'int32', 'uint32', 'uint32', EveryNCallback, TaskHandle]],
-  'DAQmxWaitForNextSampleClock': ['int32', [TaskHandle, 'double', 'bool*']]
+  'DAQmxGetDevAIPhysicalChans': ['int32', ['string', 'char**', 'ulong']],
+  'DAQmxGetSysDevNames': ['int32', ['char**', 'int32']],
+  'DAQmxGetSysNIDAQMajorVersion': ['int32', ['int*']],
+  'DAQmxGetSysNIDAQMinorVersion': ['int32', ['int*']],
+  'DAQmxCreateTask': ['int32', ['string', 'void*']],
+  'DAQmxCreateAIVoltageChan': ['int32', ['void*', 'string', 'string', 'int32', 'double', 'double', 'int32', 'string']],
+  'DAQmxCreateDOChan': ['int32', ['void*', 'string', 'string', 'int32']],
+  'DAQmxCfgSampClkTiming': ['int32', ['void*', 'string', 'double', 'int32', 'int32', 'uint64']],
+  'DAQmxStartTask': ['int32', ['void*']],
+  'DAQmxReadAnalogF64': ['int32', ['void*', 'int32', 'double', 'int32', DoubleArray, 'uint32', 'int32*', 'uint32']],
+  'DAQmxWriteDigitalScalarU32': ['int32', ['void*', 'bool', 'double', 'int32', 'bool*']],
+  'DAQmxReadDigitalScalarU32': ['int32', ['void*', 'double', 'int32*', 'bool*']],
+  'DAQmxRegisterEveryNSamplesEvent': ['int32', ['void*', 'int32', 'uint32', 'uint32', EveryNCallback, 'void*']],
+  'DAQmxWaitForNextSampleClock': ['int32', ['void*', 'double', 'bool*']]
 });
 
 var dict = {};
@@ -54,7 +49,6 @@ wss.on('connection', function (ws) {
     console.log('client connected');
     ws.on('close', function () {
         console.log('client disconnected');
-        clearInterval(id);
     });
     ws.on('message', function (msg) {
         try
@@ -101,7 +95,7 @@ wss.on('connection', function (ws) {
                     ws.send(JSON.stringify(response));
                     break;
                 case 'createAIVoltageChan':
-                    console.log(niDAQmx.DAQmxCreateAIVoltageChan(dict[msg.data.taskID], msg.data.device, msg.data.nameToAssignChannel, msg.data.terminalConfig, msg.data.minVal, msg.data.maxVal, msg.data.units, ref.NULL));
+                    niDAQmx.DAQmxCreateAIVoltageChan(dict[msg.data.taskID], msg.data.device, msg.data.nameToAssignChannel, msg.data.terminalConfig, msg.data.minVal, msg.data.maxVal, msg.data.units, ref.NULL);
                     break;
                 case 'cfgSampClkTiming':
                     niDAQmx.DAQmxCfgSampClkTiming(dict[msg.data.taskID], msg.data.source, msg.data.rate, msg.data.activeEdge, msg.data.sampleMode, msg.data.sampsPerChanToAcquire);
@@ -115,7 +109,6 @@ wss.on('connection', function (ws) {
                     read.type = ref.types.int32;
 
                     niDAQmx.DAQmxReadAnalogF64(dict[msg.data.taskID], msg.data.numSampsPerChan, msg.data.timeout, msg.data.fillMode, data, msg.data.arraySizeInSamps, read, ref.NULL);
-                    //console.log(data.toArray());
                     var response = {
                         'type': 'readAnalogF64Response',
                         'data': data.toArray().toString()
@@ -127,11 +120,11 @@ wss.on('connection', function (ws) {
                     break;
                 case 'writeDigitalScalarU32':
                     niDAQmx.DAQmxWriteDigitalScalarU32(dict[msg.data.taskID], msg.data.autoStart, msg.data.timeout, msg.data.dataLayout, ref.NULL);
-                    /*var response = {
+                    var response = {
                         'type': 'writeDigitalScalarU32Response',
                         'data': 'none'
                     }
-                    ws.send(JSON.stringify(response));*/
+                    ws.send(JSON.stringify(response));
                     break;
                 case 'readDigitalScalarU32':
                     var read = new Buffer(1000);
